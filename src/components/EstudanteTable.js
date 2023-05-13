0be3da4c4df095991d6bd51fa52fcc23520cca14
Elone <elonesampaio@gmail.com>
2023-05-12 23:40:06 -0300

    docker compose

--- src/components/EstudanteTable.js
+++ src/components/EstudanteTable.js
@@ -23,25 +23,25 @@ const EstudanteTable = () => {
   }, []);
 
   const fetchData = async () => {
-    const result = await axios("http://127.0.0.1:8000/api/estudantes");
+    const result = await axios("http://31.220.31.176:8000/api/estudantes");
     setData(result.data);
   };
 
   const botaoAddStudent = async student => {
-    await axios.post("http://127.0.0.1:8000/api/estudantes", student);
+    await axios.post("http://31.220.31.176:8000/api/estudantes", student);
     fetchData();
     setFormVisible(false);
   };
 
   const botaoUpdateStudent = async student => {
-    await axios.put(`http://127.0.0.1:8000/api/estudantes/${selectedStudent.id}`, student);
+    await axios.put(`http://31.220.31.176:8000/api/estudantes/${selectedStudent.id}`, student);
     fetchData();
     setSelectedStudent(null);
     setFormVisible(false);
   };
 
   const botaoDeleteStudent = async id => {
-    await axios.delete(`http://127.0.0.1:8000/api/estudantes/${id}`);
+    await axios.delete(`http://31.220.31.176:8000/api/estudantes/${id}`);
     fetchData();
   };
 
@@ -73,7 +73,7 @@ const EstudanteTable = () => {
       {
         Header: "Foto",
         accessor: "foto",
-        Cell: ({ value }) => <img src={`http://127.0.0.1:8000/${value}`} alt="Foto" width="50" />,
+        Cell: ({ value }) => <img src={`http://31.220.31.176:8000/${value}`} alt="Foto" width="50" />,
       },
       {
         Header: "actions"
